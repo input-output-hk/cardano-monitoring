db3eea1430233e3cf60398f67cd3aa3df00ca7b5
John Lotoski <john.lotoski@iohk.io>
2023-12-20 14:58:37 -0600

    fix: system timesync status in node exporter time checks

--- flake/nixosModules/common.nix
+++ flake/nixosModules/common.nix
@@ -114,7 +114,12 @@
     };
 
     services = {
-      chrony.enable = true;
+      chrony = {
+        enable = true;
+        extraConfig = "rtcsync";
+        enableRTCTrimming = false;
+      };
+
       cron.enable = true;
       fail2ban.enable = true;
 
